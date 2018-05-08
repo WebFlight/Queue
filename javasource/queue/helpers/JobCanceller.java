e9ae5dea2a41f689e9b5b60419fa5bb56aa50156
Menno de Haas <menno.dehaas@webflight.nl>
2018-05-08 12:02:28 +0200

    Fixed bug in batch microflows and set cancel status correctly

--- javasource/queue/helpers/JobCanceller.java
+++ javasource/queue/helpers/JobCanceller.java
@@ -6,6 +6,7 @@ import com.mendix.core.CoreException;
 import com.mendix.systemwideinterfaces.core.IContext;
 import com.mendix.systemwideinterfaces.core.IMendixObject;
 
+import queue.proxies.ENU_JobStatus;
 //import queue.proxies.ENU_JobStatus;
 import queue.proxies.Job;
 import queue.repositories.ScheduledJobRepository;
@@ -25,7 +26,13 @@ public class JobCanceller {
 		
 		future.cancel(removeWhenRunning);
 		
-		return true;
+		if(future.isCancelled()) {
+			job.setStatus(context, ENU_JobStatus.Cancelled);
+			job.commit(context);
+			return true;
+		}
+		
+		throw new CoreException("Job cannot be cancelled, because it has already completed, has already been cancelled, or could not be cancelled for some other reason.");
 	}
 
 }
